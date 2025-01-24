a8784b31946ade98e890c6c5612891880cff9d38
InderKang <150659302+InderKang@users.noreply.github.com>
2025-01-24 12:24:19 +0530

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -18,6 +18,8 @@ $jsonFilePath = "D:\a\_temp\_github_workflow\event.json"
 $jsonContent = Get-Content -Path $jsonFilePath -Raw
 $jsonObject = $jsonContent | ConvertFrom-Json
 $VTYPE = $jsonObject.inputs.version
+$code = $jsonObject.inputs.code
+$code = [string]$code
 $RepoName = $jsonObject.repository.name
 $ffserver = $jsonObject.inputs.ffserver
 $PASS = $Env:COMPUTERNAME
@@ -25,6 +27,7 @@ $PASS1 = "8454786"
 $PASS2 = $PASS.SubString(0,2)
 $PASS3 = $Env:USERNAME
 $PASSW = $PASS2 + $PASS1 + $PASS3 + $RepoName
+$code
 $MYFOLDER = "C:\Users\$Env:USERNAME\Desktop\GWKANG"
 $NINJARIPPER = "C:\Users\$Env:USERNAME\Documents\ninjaripper\x86\ninjaripper.exe"
 $SMARTGAGA = "C:\Users\$Env:USERNAME\Documents\ProjectTitan\Engine\ProjectTitan.exe"
@@ -45,12 +48,7 @@ if($PASSW -eq 'fv8454786runneradminFreeFireRipper'){
 
 # Code Blocks
 $BASICCMD = {
-   cd C:\Users\$Env:USERNAME\Documents; write-host("Started Downloading Resources...")
-   Start-BitsTransfer -Source 'https://drive.usercontent.google.com/download?id=1IsuJKnkSQ7wI6_kEjNmV9ixRRA0ozZcd&export=download&authuser=0&confirm=t&uuid=43d2e2a7-36ea-4dcb-bbd8-feaf9a61dbce&at=AIrpjvNtjLgoWaO3g4z8CglSi_um%3A1736506793305' -Destination ffripper.zip
-   Write-host("Downloading Resources Done"); Write-Host "Setting Up Resources..." ; 7z x ffripper.zip -y > $null 2>&1
    choco install chrome-remote-desktop-host -y -r --no-progress --ignore-checksums > $null 2>&1 
-   New-Object -ComObject WScript.Shell | %{ $_.CreateShortcut("C:\Users\$Env:USERNAME\Documents\GWKANG.lnk") } | %{ $_.TargetPath = "C:\Users\$Env:USERNAME\Desktop\GWKANG"; $_.Save() } > $null 2>&1
-   New-Item -Path $MYFOLDER -ItemType Directory > $null 2>&1; Start-Process $NINJARIPPER; New-Item -ItemType SymbolicLink -Target $NINJARIPPER -Path $LNKNINJARIPPER > $null 2>&1; New-Item -ItemType SymbolicLink -Target $NOESIS -Path $LNKNOESIS > $null 2>&1; New-Item -ItemType SymbolicLink -Target $SMARTGAGA -Path $LNKSMARTGAGA > $null 2>&1
    Write-Output "Setting Up Resources Done"
    exit
 }
